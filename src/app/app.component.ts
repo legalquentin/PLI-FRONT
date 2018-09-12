c66114edca48802617897ac419c9d40dadc81aa5
quentin <legal_q@etna-alternance.net>
2018-09-12 11:28:43 +0200

    [F|all@master] Remade app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Language, TranslationService, LocaleService } from 'angular-l10n';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @Language() lang: string;
+
+  constructor(
+    private router: Router,
+    private translation: TranslationService,
+    private locale: LocaleService) {
+  }
   title = 'cryptobo4rd';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,85 @@
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {
+  L10nConfig,
+  L10nLoader,
+  LocalizationModule,
+  StorageStrategy,
+  ProviderType
+} from 'angular-l10n';
 
+// APP
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material.module';
+// SERVICES
+
+// COMPONENTS
+import { CbCryptobo4rdComponent } from './cb-cryptobo4rd/cb-cryptobo4rd.component';
+import { CbDashboardComponent } from './cb-dashboard/cb-dashboard.component';
+import { CbMenuLeftComponent } from './cb-menu-left/cb-menu-left.component';
+import { CbMenuTopComponent } from './cb-menu-top/cb-menu-top.component';
+import { CbConnectionComponent } from './cb-connection/cb-connection.component';
+import { CbWidget1Component } from './cb-dashboard/cb-widgets/cb-widget-1/cb-widget-1.component';
+import { CbKpi1Component } from './cb-dashboard/cb-kpis/cb-kpi-1/cb-kpi-1.component';
+
+const l10nConfig: L10nConfig = {
+  locale: {
+        languages: [
+            { code: 'en', dir: 'ltr' },
+            { code: 'fr', dir: 'ltr' }
+        ],
+        language: 'en',
+        storage: StorageStrategy.Local,
+        cookieExpiration: 30
+    },
+    translation: {
+        providers: [
+            { type: ProviderType.Static, prefix: './assets/locale-' }
+        ],
+        caching: true,
+        missingValue: 'No key'
+    }
+};
+
+// Advanced initialization.
+export function initL10n(l10nLoader: L10nLoader): Function {
+  return () => l10nLoader.load();
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CbCryptobo4rdComponent,
+    CbDashboardComponent,
+    CbMenuLeftComponent,
+    CbMenuTopComponent,
+    CbConnectionComponent,
+    CbWidget1Component,
+    CbKpi1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    LocalizationModule.forRoot(l10nConfig),
+    BrowserAnimationsModule,
+    NgxChartsModule,
+    FlexLayoutModule,
+    AppMaterialModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initL10n,
+      deps: [L10nLoader],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
