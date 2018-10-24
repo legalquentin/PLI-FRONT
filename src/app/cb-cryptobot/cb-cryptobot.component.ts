a7045bd2cb673cbec0e73e2edb64062d66c27b4a
nwcra791 <labego_s@etna-alternance.net>
2018-10-24 14:46:50 +0200

    crypto-bot add icon and status

--- src/app/cb-cryptobot/cb-cryptobot.component.ts
+++ src/app/cb-cryptobot/cb-cryptobot.component.ts
@@ -12,6 +12,12 @@ export class CbCryptobotComponent implements OnInit {
   lang: string;
   @ViewChild('testTemplate')
   textTemplate: TemplateRef<any>;
+  @ViewChild('sliderTemplate')
+  sliderTemplate: TemplateRef<any>;
+  @ViewChild('iconButtonTemplate')
+  iconButtonTemplate: TemplateRef<any>;
+  @ViewChild('iconStatTemplate')
+  iconStatTemplate: TemplateRef<any>;
 
 public LOADED = false;
 public LOADED_TABLE2 = false;
@@ -58,7 +64,7 @@ public TRANSACTIONS = {
     });
     this.TRANSACTIONS.columns.push({
       KEY: 'ACTIVE',
-      VIEW: this.textTemplate
+      VIEW: this.iconStatTemplate
     });
     this.TRANSACTIONS.data.push({
       EXCHANGE: 'salut',
@@ -67,13 +73,25 @@ public TRANSACTIONS = {
       PRICE: '15€',
       CRYPTO: 'bitcoin',
       NUMBER: '0.002',
-      ACTIVE: 'en coure'
+      ACTIVE: 1
     });
     this.TRANSACTIONS.data.push({
-      EXCHANGE: 'salut'
+      EXCHANGE: 'salut',
+      DATE: '16/20/2004',
+      ORDER_TYPE: 'dqbdsdgbdgb',
+      PRICE: '15€',
+      CRYPTO: 'bitcoin',
+      NUMBER: '0.002',
+      ACTIVE: 2
     });
     this.TRANSACTIONS.data.push({
-      EXCHANGE: 'salut'
+      EXCHANGE: 'salut',
+      DATE: '16/20/2004',
+      ORDER_TYPE: 'dqbdsdgbdgb',
+      PRICE: '15€',
+      CRYPTO: 'bitcoin',
+      NUMBER: '0.002',
+      ACTIVE: 3
     });
     this.LOADED = true;
 
@@ -83,20 +101,43 @@ public TRANSACTIONS = {
       KEY: 'CRYPTO',
       VIEW: this.textTemplate
     });
-    this.CRYPTO_LIST.data.push({
-      CRYPTO: 'Bitcoin'
+    this.CRYPTO_LIST.columns.push({
+      KEY: 'TOTAL_HAVE',
+      VIEW: this.textTemplate
+    });
+    this.CRYPTO_LIST.columns.push({
+      KEY: 'ACTIVE',
+      VIEW: this.sliderTemplate
+    });
+    this.CRYPTO_LIST.columns.push({
+      KEY: 'DELETE',
+      VIEW: this.iconButtonTemplate
     });
     this.CRYPTO_LIST.data.push({
-      CRYPTO: 'Etherum'
+      CRYPTO: 'Bitcoin',
+      ACTIVE: true,
+      TOTAL_HAVE: '16.589',
+      DELETE: null
     });
     this.CRYPTO_LIST.data.push({
-      CRYPTO: 'LiteCoin'
+      CRYPTO: 'Etherum',
+      ACTIVE: false,
+      TOTAL_HAVE: '16.589',
+      DELETE: null
     });
     this.CRYPTO_LIST.data.push({
-      CRYPTO: 'Zcash'
+      CRYPTO: 'LiteCoin',
+      ACTIVE: true,
+      TOTAL_HAVE: '16.589',
+      DELETE: null
     });
     this.CRYPTO_LIST.data.push({
-      CRYPTO: 'Trukcoin'
+      CRYPTO: 'Zcash',
+      DELETE: null
+    });
+    this.CRYPTO_LIST.data.push({
+      CRYPTO: 'Trukcoin',
+      DELETE: null
     });
     this.LOADED_TABLE2 = true;
   }
